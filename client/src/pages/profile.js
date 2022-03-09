9e5e03a54aa4391a63c91662e19812bfa7318f07
dipesh <jaswanidipesh8@gmail.com>
2022-03-09 16:33:00 +0530

    intial commit

---
+++ client/src/pages/profile.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./profile.css";
+
+const Profile = () => {
+  return (
+  
+        
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card shadow-sm">
+          <div class="card-header bg-transparent text-center">
+            <img class="profile_img" src="https://placeimg.com/640/480/arch/any" alt=""/>
+            <h3>Ishmam Ahasan Samin</h3>
+          </div>
+          <div class="card-body">
+            <p class="mb-0"><strong class="pr-1">Student ID:</strong>321000001</p>
+            <p class="mb-0"><strong class="pr-1">Class:</strong>4</p>
+            <p class="mb-0"><strong class="pr-1">Section:</strong>A</p>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+   
+  )
+}
+
+export default Profile;
